c509212f9a50947da2705f0acacf504063a2c12c
Fatima Mahamoud <fsmahamoud@myseneca.ca>
2024-11-20 13:48:23 -0500

    Lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+
 class Time:
     """Simple object type for time of the day.
-    data attributes: hour, minute, second
+       Data attributes: hour, minute, second
     """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
+    def __init__(self, hour=12, minute=0, second=0):
+        """Constructor for time object"""
         self.hour = hour
         self.minute = minute
         self.second = second
@@ -15,16 +15,32 @@ def format_time(t):
     return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
 
 def sum_times(t1, t2):
-    """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
+    """Add two time objects and return the sum."""
+    sum = Time(0, 0, 0)
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    # Handle carryover for seconds
+    if sum.second >= 60:
+        sum.minute += sum.second // 60  # Carryover to minutes
+        sum.second = sum.second % 60  # Keep only the remainder
+
+    # Handle carryover for minutes
+    if sum.minute >= 60:
+        sum.hour += sum.minute // 60  # Carryover to hours
+        sum.minute = sum.minute % 60  # Keep only the remainder
+
+    # Ensure hour is within a valid range (optional, based on requirements)
+    if sum.hour >= 24:
+        sum.hour = sum.hour % 24  # Wrap around if the hour exceeds 24
+
     return sum
 
 def valid_time(t):
-    """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+    """Check for the validity of the time object attributes:
+       24 > hour > 0, 60 > minute > 0, 60 > second > 0 
+    """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
         return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
--- lab7a1.py
+++ lab7a1.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: fsmahamoud
+
 from lab7a import *
-t1 = Time(8,0,0)
-t2 = Time(8,55,0)
-t3 = Time(9,50,0)
 
-td = Time(0,50,0)
+# Creating Time objects
+t1 = Time(8, 0, 0)
+t2 = Time(8, 55, 0)
+t3 = Time(9, 50, 0)
 
-tsum1 = sum_times(t1,td)
-tsum2 = sum_times(t2,td)
-tsum3 = sum_times(t3,td)   
+# Time to add
+td = Time(0, 50, 0)
 
+# Sum of times
+tsum1 = sum_times(t1, td)
+tsum2 = sum_times(t2, td)
+tsum3 = sum_times(t3, td)
+
+# Formatting and printing the results
 ft = format_time
-print(ft(t1),'+',ft(td),'-->',ft(tsum1))
-print(ft(t2),'+',ft(td),'-->',ft(tsum2))
-print(ft(t3),'+',ft(td),'-->',ft(tsum3))
+print(ft(t1), '+', ft(td), '-->', ft(tsum1))  # Output the sum for t1
+print(ft(t2), '+', ft(td), '-->', ft(tsum2))  # Output the sum for t2
+print(ft(t3), '+', ft(td), '-->', ft(tsum3))  # Output the sum for t3
